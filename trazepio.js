a4f19789660df97a44c0c5db41dc228c8dbaf032
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-10-05 15:21:54 -0300

    Add files via upload
    11) Escreva um algoritmo para ler as dimensões de um trapézio (base maior, base menor e altura), calcular e escrever a área do trapézio. a=(B+b).h/2

---
+++ trazepio.js
@@ -0,0 +1,7 @@
+function multiplique(){
+    var numero1=prompt(("Digite o valor do comprimentos da base maior "));
+    var numero2=prompt(("Digite o valor do comprimentos da base menor"));
+    var numero3=prompt(("Digite a altura "));
+    var multiplique=(numero1+numero2)*numero3/2
+    alert("A área do trapezio é :"+multiplique);
+    }
